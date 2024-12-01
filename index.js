dd618d8cb5f439e11971eb4574092dd90015adf8
Akhilesh S Kumbar <akhilkumbar@outlook.com>
2024-12-01 15:18:36 -0500

    Update: env variable

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+const AWS = require("aws-sdk");
+const secretsManager = new AWS.SecretsManager();
+
+async function getEmailCredentials() {
+  const secret = await secretsManager
+    .getSecretValue({ SecretId: process.env.EMAIL_SECRET_ID })
+    .promise();
+  return JSON.parse(secret.SecretString);
+}
+
 const { sendVerificationEmail } = require("./utils/emailService");
-const verifyEmailLink = process.env.VERIFY_EMAIL_LINK;
 
 exports.handler = async (event) => {
+  const emailCredentials = await getEmailCredentials();
+  const apiKey = emailCredentials.apiKey;
+  const domain = emailCredentials.domain;
+  const fromEmail = emailCredentials.fromEmail;
+  const verify_email_link = emailCredentials.verify_email_link;
+  const verify_email_expiry_time = emailCredentials.verify_email_expiry_time;
+
   try {
     const snsMessage = event.Records[0].Sns.Message;
     const { email, firstName, lastName, verificationToken } =
       JSON.parse(snsMessage);
-    const verificationLink = `${verifyEmailLink}/${verificationToken}`;
+    const verificationLink = `${verify_email_link}/${verificationToken}`;
     console.log("verificationLink", verificationLink);
-    await sendVerificationEmail(email, verificationLink);
+    await sendVerificationEmail(
+      email,
+      verificationLink,
+      apiKey,
+      domain,
+      fromEmail
+    );
     return {
       statusCode: 200,
       body: JSON.stringify({
